f94aaa4db5237ebf12568020f476955e77a75268
zineddine <eino1209@gmail.com>
2023-03-13 22:02:49 +0100

    first setup of the server

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# /generate-csr : GET
+@app.route('/generate-csr')
+def generate_csr():
+    return render_template('generate-csr.html')
+
+
+# /generate-certificate : POST
+# params: 
+#   Common Name: string
+#   Organization: string
+#   Country: string (two letter code)
+#   State: string
+#   City: string
+
+@app.route('/generate-certificate', methods=['POST'])
+def generate_certificate():
+    common_name = request.form['common_name']
+    organization = request.form['organization']
+    country = request.form['country']
+    state = request.form['state']
+    city = request.form['city']
+
+    # TODO: generate certificate
+
+    return redirect(url_for('index'))
