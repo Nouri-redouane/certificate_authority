0d49ae920562a2d17162f59e3d65dce2d8e9e28a
Nouri-redouane <Radouane.nouri0@gmail.com>
2023-03-18 20:34:27 +0100

    done
    done

--- verify.py
+++ verify.py
@@ -3,24 +3,22 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
 
-cert_file = open('')
+cert_file = open("entity.crt", "rb")
 cert_data = cert_file.read()
-cert = x509.load_der_x509_certificate(data=cert_data)
+cert = x509.load_pem_x509_certificate(data=cert_data)
 
 # chain contains the Let's Encrypt certificate 
-chain_file = open('')
+chain_file = open("ca.crt", "rb")
 chain_data = chain_file.read()
-chain = x509.load_pem_x509_certificate(data=chain_data, backend=default_backend())
+chain = x509.load_pem_x509_certificate(data=chain_data)
 
 public_key = chain.public_key()
-
-verifier = public_key.verifier(
+print(cert.signature)
+verifier = public_key.verify(
                 signature = cert.signature,
-                padding = padding.PSS(
-                              mgf = padding.MGF1(hashes.SHA256()),
-                              salt_length = padding.PSS.MAX_LENGTH),
+                data=cert_data,
+                padding = padding.PKCS1v15(),
                 algorithm = hashes.SHA256())
-
 verifier.update(cert.tbs_certificate_bytes)
 verifier.verify()
 
