8c3d557c813966abb21dbcfcd6d676cf1d5b4df6
sekhriabdelali <84051665+sekhriabdelali@users.noreply.github.com>
2023-05-31 10:58:41 +0200

    Update verify.py

--- verify.py
+++ verify.py
@@ -1,17 +1,21 @@
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography import x509
+from datetime import datetime
 
 def verify(filename):
-
     cert_to_check = x509.load_pem_x509_certificate(data=open("uploads/"+filename, "rb").read())
     ca_cer = x509.load_pem_x509_certificate(data=open("ca.crt", "rb").read())
     issuer_public_key = ca_cer.public_key()
 
+    # Vérification de la période de validité
+    now = datetime.now()
+    if now < cert_to_check.not_valid_before or now > cert_to_check.not_valid_after:
+        return False
+    
     try:
         issuer_public_key.verify(
             cert_to_check.signature,
             cert_to_check.tbs_certificate_bytes,
-            # Depends on the algorithm used to create the certificate
             padding.PKCS1v15(),
             cert_to_check.signature_hash_algorithm,
         )
